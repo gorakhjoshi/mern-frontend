73979e6ec3ea017f06dc5a37b93033268001bff8
Gorakh Raj Joshi <59221046+gorakhjoshi@users.noreply.github.com>
2024-02-28 11:58:17 +0545

    env changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  FIREBASE_KEY: process.env.VITE_FIREBASE_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT: process.env.VITE_FIREBASE_PROJECT,
+  FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  FIREBASE_SENDER_ID: process.env.VITE_FIREBASE_SENDER_ID,
+  FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
+  API_URL: process.env.VITE_API_URL,
 })
